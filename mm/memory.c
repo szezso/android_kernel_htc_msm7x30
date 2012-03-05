3d8db72db7a5ff82c109461555b8f4775dc7bda0
Al Viro <viro@zeniv.linux.org.uk>
2012-03-05 13:25:09 -0500

    VM: unmap_page_range() can return void
    return value is always the 4th ('end') argument.
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- mm/memory.c
+++ mm/memory.c
@@ -1272,10 +1272,10 @@ static inline unsigned long zap_pud_range(struct mmu_gather *tlb,
 	return addr;
 }
 
-static unsigned long unmap_page_range(struct mmu_gather *tlb,
-				struct vm_area_struct *vma,
-				unsigned long addr, unsigned long end,
-				struct zap_details *details)
+static void unmap_page_range(struct mmu_gather *tlb,
+			     struct vm_area_struct *vma,
+			     unsigned long addr, unsigned long end,
+			     struct zap_details *details)
 {
 	pgd_t *pgd;
 	unsigned long next;
@@ -1295,8 +1295,6 @@ static unsigned long unmap_page_range(struct mmu_gather *tlb,
 	} while (pgd++, addr = next, addr != end);
 	tlb_end_vma(tlb, vma);
 	mem_cgroup_uncharge_end();
-
-	return addr;
 }
 
 #ifdef CONFIG_PREEMPT
@@ -1372,10 +1370,9 @@ unsigned long unmap_vmas(struct mmu_gather *tlb,
 				 */
 				if (vma->vm_file)
 					unmap_hugepage_range(vma, start, end, NULL);
-
-				start = end;
 			} else
-				start = unmap_page_range(tlb, vma, start, end, details);
+				unmap_page_range(tlb, vma, start, end, details);
+			start = end;
 		}
 	}
 
