3ab2a555ae70cf6bb9b18de3222e5b681a0d936b
Pavankumar Kondeti <pkondeti@codeaurora.org>
2012-06-15 10:59:05 +0530

    USB: android: strim serial number buffer
    User space writes USB serial number to a sysfs file.  Strim the
    buffer before copying to USB string descriptor, otherwise
    serial number may contain white spaces or new line characters.
    
    Change-Id: If0dc699f53b92d80beaa099148f085d6c63af9a6
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    
    Conflicts:
    
    	drivers/usb/gadget/android.c

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -1972,7 +1972,8 @@ field ## _store(struct device *dev, struct device_attribute *attr,	\
 {									\
 	pr_info("%s: %s\n", __func__, buf);				\
 	if (size >= sizeof(buffer)) return -EINVAL;			\
-	return strlcpy(buffer, buf, sizeof(buffer));			\
+	strlcpy(buffer, strim((char *) buf), sizeof(buffer));		\
+	return size;							\
 }									\
 static DEVICE_ATTR(field, S_IRUGO | S_IWUSR, field ## _show, field ## _store);
 
