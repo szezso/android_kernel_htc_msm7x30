1284edac3730850467c31934a372b7c1ce718450
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2015-01-11 20:17:40 +0200

    f2fs: use umode_t for f2fs_[create|mknod]
    Change-Id: I53fd3813495dcf534ecb2c1327b959a0ff129fd5

--- fs/f2fs/namei.c
+++ fs/f2fs/namei.c
@@ -108,7 +108,7 @@ static inline void set_cold_files(struct f2fs_sb_info *sbi, struct inode *inode,
 	}
 }
 
-static int f2fs_create(struct inode *dir, struct dentry *dentry, int mode,
+static int f2fs_create(struct inode *dir, struct dentry *dentry, umode_t mode,
 						struct nameidata *nd)
 {
 	struct super_block *sb = dir->i_sb;
@@ -348,7 +348,7 @@ static int f2fs_rmdir(struct inode *dir, struct dentry *dentry)
 }
 
 static int f2fs_mknod(struct inode *dir, struct dentry *dentry,
-				int mode, dev_t rdev)
+				umode_t mode, dev_t rdev)
 {
 	struct super_block *sb = dir->i_sb;
 	struct f2fs_sb_info *sbi = F2FS_SB(sb);
