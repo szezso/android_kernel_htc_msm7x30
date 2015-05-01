d929195e4f1cf6a9105eb0c410ebbf52eb068bb3
jonas2515 <jonas2515@users.noreply.github.com>
2015-05-01 15:48:51 +0200

    mach-msm: primou: Use default HTC audio parameters * With fix for loud earpiece

--- arch/arm/mach-msm/board-primou-audio.c
+++ arch/arm/mach-msm/board-primou-audio.c
@@ -51,25 +51,25 @@ static struct vreg *vreg_audio_n1v8;
 
 static struct q5v2_hw_info q5v2_audio_hw[Q5V2_HW_COUNT] = {
 	[Q5V2_HW_HANDSET] = {
-		.max_gain[VOC_NB_INDEX] = -600,
+		.max_gain[VOC_NB_INDEX] = -800,
 		.min_gain[VOC_NB_INDEX] = -1400,
-		.max_gain[VOC_WB_INDEX] = -600,
+		.max_gain[VOC_WB_INDEX] = -800,
 		.min_gain[VOC_WB_INDEX] = -1400,
 	},
 	[Q5V2_HW_HEADSET] = {
-		.max_gain[VOC_NB_INDEX] = 800,
-		.min_gain[VOC_NB_INDEX] = -1400,
-		.max_gain[VOC_WB_INDEX] = 800,
-		.min_gain[VOC_WB_INDEX] = -1400,
+		.max_gain[VOC_NB_INDEX] = 0,
+		.min_gain[VOC_NB_INDEX] = -2000,
+		.max_gain[VOC_WB_INDEX] = 0,
+		.min_gain[VOC_WB_INDEX] = -2000,
 	},
 	[Q5V2_HW_SPEAKER] = {
-		.max_gain[VOC_NB_INDEX] = 1000,
+		.max_gain[VOC_NB_INDEX] = 600,
 		.min_gain[VOC_NB_INDEX] = -900,
-		.max_gain[VOC_WB_INDEX] = 1000,
+		.max_gain[VOC_WB_INDEX] = 600,
 		.min_gain[VOC_WB_INDEX] = -900,
 	},
 	[Q5V2_HW_BT_SCO] = {
-		.max_gain[VOC_NB_INDEX] = 800,
+		.max_gain[VOC_NB_INDEX] = 0,
 		.min_gain[VOC_NB_INDEX] = -1500,
 		.max_gain[VOC_WB_INDEX] = 0,
 		.min_gain[VOC_WB_INDEX] = -1500,
