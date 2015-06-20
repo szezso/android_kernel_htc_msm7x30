196ca5211781d5e64c0e030b133e5a3ce66907f0
Szeleczky Zsombor <szezsoo@gmail.com>
2015-06-20 19:05:59 +0200

    Several updates

--- arch/arm/mach-msm/board-vivo.c
+++ arch/arm/mach-msm/board-vivo.c
@@ -176,20 +176,6 @@ unsigned int vivo_get_engineerid(void)
 		(((pull) & 0x3) << 15)          | \
 		(((drvstr) & 0xF) << 17))
 
-
-static void config_gpio_table(uint32_t *table, int len)
-{
-	int n, rc;
-	for (n = 0; n < len; n++) {
-		rc = gpio_tlmm_config(table[n], GPIO_CFG_ENABLE);
-		if (rc) {
-			pr_err("[CAM] %s: gpio_tlmm_config(%#x)=%d\n",
-				__func__, table[n], rc);
-			break;
-		}
-	}
-}
-
 #if 0
 #endif
 
