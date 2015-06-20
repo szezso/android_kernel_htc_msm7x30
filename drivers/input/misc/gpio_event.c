9405c0a9d7e83e7d77da2b5e1a672e91ea850867
Szeleczky Zsombor <szezsoo@gmail.com>
2015-06-20 19:28:49 +0200

    Moar build fixes

--- drivers/input/misc/gpio_event.c
+++ drivers/input/misc/gpio_event.c
@@ -20,11 +20,9 @@
 #include <linux/hrtimer.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
+
 #ifdef CONFIG_TOUCHSCREEN_ATMEL_SWEEP2WAKE
 #include <linux/atmel_qt602240.h>
-#endif
-#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_SWEEP2WAKE
-#include <linux/synaptics_i2c_rmi.h>
 #endif
 
 struct gpio_event {
@@ -48,7 +46,7 @@ static int gpio_input_event(
 		if (ip->input_devs->dev[devnr] == dev)
 			break;
 	if (devnr == ip->input_devs->count) {
-		pr_err("gpio_input_event: unknown device %p\n", dev);
+		KEY_LOGE("KEY_ERR: %s: unknown device %p\n", __func__, dev);
 		return -EIO;
 	}
 
@@ -75,7 +73,7 @@ static int gpio_event_call_all_func(struct gpio_event *ip, int func)
 		for (i = 0; i < ip->info->info_count; i++, ii++) {
 			if ((*ii)->func == NULL) {
 				ret = -ENODEV;
-				pr_err("gpio_event_probe: Incomplete pdata, "
+				KEY_LOGE("KEY_ERR: gpio_event_probe: Incomplete pdata, "
 					"no function\n");
 				goto err_no_func;
 			}
@@ -84,7 +82,7 @@ static int gpio_event_call_all_func(struct gpio_event *ip, int func)
 			ret = (*ii)->func(ip->input_devs, *ii, &ip->state[i],
 					  func);
 			if (ret) {
-				pr_err("gpio_event_probe: function failed\n");
+				KEY_LOGE("KEY_ERR: gpio_event_probe: function failed\n");
 				goto err_func_failed;
 			}
 		}
@@ -136,12 +134,12 @@ static int gpio_event_probe(struct platform_device *pdev)
 
 	event_info = pdev->dev.platform_data;
 	if (event_info == NULL) {
-		pr_err("gpio_event_probe: No pdata\n");
+		KEY_LOGE("KEY_ERR: %s: No pdata\n", __func__);
 		return -ENODEV;
 	}
 	if ((!event_info->name && !event_info->names[0]) ||
 	    !event_info->info || !event_info->info_count) {
-		pr_err("gpio_event_probe: Incomplete pdata\n");
+		KEY_LOGE("KEY_ERR: %s: Incomplete pdata\n", __func__);
 		return -ENODEV;
 	}
 	if (!event_info->name)
@@ -153,7 +151,7 @@ static int gpio_event_probe(struct platform_device *pdev)
 		     sizeof(ip->input_devs->dev[0]) * dev_count, GFP_KERNEL);
 	if (ip == NULL) {
 		err = -ENOMEM;
-		pr_err("gpio_event_probe: Failed to allocate private data\n");
+		KEY_LOGE("KEY_ERR: %s: Failed to allocate private data\n", __func__);
 		goto err_kp_alloc_failed;
 	}
 	ip->input_devs = (void*)&ip->state[event_info->info_count];
@@ -163,8 +161,8 @@ static int gpio_event_probe(struct platform_device *pdev)
 		struct input_dev *input_dev = input_allocate_device();
 		if (input_dev == NULL) {
 			err = -ENOMEM;
-			pr_err("gpio_event_probe: "
-				"Failed to allocate input device\n");
+			KEY_LOGE("KEY_ERR: %s: "
+				"Failed to allocate input device\n", __func__);
 			goto err_input_dev_alloc_failed;
 		}
 		input_set_drvdata(input_dev, ip);
@@ -173,30 +171,10 @@ static int gpio_event_probe(struct platform_device *pdev)
 		input_dev->event = gpio_input_event;
 		ip->input_devs->dev[i] = input_dev;
 #ifdef CONFIG_TOUCHSCREEN_ATMEL_SWEEP2WAKE
-#ifdef CONFIG_MACH_SAGA
-		if (!strcmp(input_dev->name, "saga-keypad")) {
-			sweep2wake_saga_setdev(input_dev);
-#endif
-#ifdef CONFIG_MACH_SPADE
-		if (!strcmp(input_dev->name, "spade-keypad")) {
+		if (!strcmp(input_dev->name, "vivo-keypad")) {
 			sweep2wake_atmel_setdev(input_dev);
-#endif
 			printk(KERN_INFO "[sweep2wake]: set device %s\n", input_dev->name);
 		}
-#endif
-#ifdef CONFIG_TOUCHSCREEN_SYNAPTICS_SWEEP2WAKE
-#ifdef CONFIG_MACH_SPADE
-		if (!strcmp(input_dev->name, "spade-keypad")) {
-			sweep2wake_syn_setdev(input_dev);
-			printk(KERN_INFO "[sweep2wake]: set device %s\n", input_dev->name);
-		}
-#endif
-#ifdef CONFIG_MACH_PRIMOU
-		if (!strcmp(input_dev->name, "primou-keypad")) {
-			sweep2wake_syn_setdev(input_dev);
-			printk(KERN_INFO "[sweep2wake]: set device %s\n", input_dev->name);
-		}
-#endif
 #endif
 	}
 	ip->input_devs->count = dev_count;
@@ -218,8 +196,8 @@ static int gpio_event_probe(struct platform_device *pdev)
 	for (i = 0; i < dev_count; i++) {
 		err = input_register_device(ip->input_devs->dev[i]);
 		if (err) {
-			pr_err("gpio_event_probe: Unable to register %s "
-				"input device\n", ip->input_devs->dev[i]->name);
+			KEY_LOGE("KEY_ERR: %s: Unable to register %s "
+				"input device\n", __func__, ip->input_devs->dev[i]->name);
 			goto err_input_register_device_failed;
 		}
 		registered++;
